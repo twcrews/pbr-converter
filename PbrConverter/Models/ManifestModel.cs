c275b20ff08cdbbbe4cc91511b546ccbb423d782
Thomas Crews <twcrews96@gmail.com>
2020-10-23 16:07:40 -0500

    Issue 2: Migrated existing models to standalone files. Added ConfigurationModel.cs to represent settings.json.

---
+++ PbrConverter/Models/ManifestModel.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Crews.Utility.PbrConverter.Models
+{
+    public class ManifestModel
+    {
+        [JsonProperty("format_version")]
+        public int FormatVersion { get; set; }
+        [JsonProperty("header")]
+        public ManifestInfo Header { get; set; }
+        [JsonProperty("modules")]
+        public List<ManifestInfo> Modules { get; set; }
+        [JsonProperty("capabilities")]
+        public string[] Capabilities { get; set; }
+
+        public class ManifestInfo
+        {
+            [JsonProperty("description")]
+            public string Description { get; set; }
+            [JsonProperty("name")]
+            public string Name { get; set; }
+            [JsonProperty("type")]
+            public string Type { get; set; }
+            [JsonProperty("uuid")]
+            public string UUID { get; set; }
+            public int[] Version { get; set; }
+            [JsonProperty("min_engine_version")]
+            public int[] MinEngineVersion { get; set; }
+        }
+    }
+}
---
+++ PbrConverter/Models/TextureSetModel.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Crews.Utility.PbrConverter.Models
+{
+    public class TextureSetModel
+    {
+        [JsonProperty("format_version")]
+        public string FormatVersion { get; set; }
+        [JsonProperty("minecraft:texture_set")]
+        public TextureSetInfo MinecraftTextureSet { get; set; }
+
+        public class TextureSetInfo
+        {
+            [JsonProperty("color")]
+            public object Color { get; set; }
+            [JsonProperty("metalness_emissive_roughness")]
+            public object MER { get; set; }
+            [JsonProperty("normal")]
+            public object Normal { get; set; }
+        }
+    }
+}
