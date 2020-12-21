22fc3683e8d8fd236ff40dcaac91129bf0061b7c
Tommy Crews <twcrews96@gmail.com>
2020-12-21 15:50:00 -0600

    Added MessageWindow to handle exceptions.

---
+++ PbrConverter/MessageWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+
+namespace Crews.Utility.PbrConverter
+{
+    /// <summary>
+    /// Interaction logic for MessageWindow.xaml
+    /// </summary>
+    public partial class MessageWindow : Window
+    {
+        /// <summary>
+        /// The message caption.
+        /// </summary>
+        public string Caption
+        {
+            get => CaptionTextBlock.Text;
+            set => CaptionTextBlock.Text = value;
+        }
+
+        /// <summary>
+        /// The message text.
+        /// </summary>
+        public string Message
+        {
+            get => MessageTextBlock.Text;
+            set => MessageTextBlock.Text = value;
+        }
+
+        public MessageWindow() => InitializeComponent();
+        private void DismissWindow(object sender, RoutedEventArgs e) => Close();
+    }
+}
